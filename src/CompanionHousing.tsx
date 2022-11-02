9cf163010d4660499d86b29503870303d0e3fe00
ethanchoi1 <ethanchoi19@gmail.com>
2022-11-02 04:13:41 -0700

    Basic features and placeholders for new tab page

---
+++ src/CompanionHousing.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+// import './App.css';
+
+function CompanionHousing() {
+  return (
+    <div className="flex flex-col font-mono h-full items-center justify-center text-2xl text-white w-full">
+      <div className="aspect-square bg-secondary-bg justify-center rounded-full text-center text-inherit w-9/12">
+        dinosaur
+      </div>
+    </div>
+  );
+}
+
+export default CompanionHousing;
