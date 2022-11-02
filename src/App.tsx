4b4a0ab1de3543c5b85d477f449336cbebb07e14
borajoo <chocobora1@gmail.com>
2022-11-02 16:25:36 -0700

    Reorganize into components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import './App.css';
-import Navbar from './Navbar';
-import CompanionHousing from './CompanionHousing';
-import ResourcesLink from './ResourcesLink';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import CompanionHousing from "./components/CompanionHousing";
+import ResourcesLink from "./components/ResourcesLink";
 
 function App() {
   return (
-    <div className="flex items-stretch h-screen">
-        <div className="border-solid flex-auto pl-[2.5%] pt-[2.5%] w-3/12 ">
-            <Navbar />
-        </div>
-        <div className="border-solid flex-auto w-6/12">
-            <CompanionHousing />
-        </div>
-        <div className="border-solid flex-auto pb-[2.5%] pr-[2.5%] w-3/12">
-            <ResourcesLink />
-        </div>
+    <div className='flex items-stretch h-screen'>
+      <div className='border-solid flex-auto pl-[2.5%] pt-[2.5%] w-3/12 '>
+        <Navbar />
+      </div>
+      <div className='border-solid flex-auto w-6/12'>
+        <CompanionHousing />
+      </div>
+      <div className='border-solid flex-auto pb-[2.5%] pr-[2.5%] w-3/12'>
+        <ResourcesLink />
+      </div>
     </div>
   );
 }
