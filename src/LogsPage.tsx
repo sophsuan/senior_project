731b1852cccd68db6890cf690b4356180be818aa
borajoo <chocobora1@gmail.com>
2023-03-19 13:15:36 -0700

    Change colors of logs

--- src/LogsPage.tsx
+++ src/LogsPage.tsx
@@ -2,7 +2,7 @@ import { useEffect, useContext } from "react";
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
-  XIcon,
+  XIcon
 } from "@heroicons/react/solid";
 import {
   add,
@@ -15,7 +15,7 @@ import {
   isToday,
   parse,
   parseISO,
-  startOfToday,
+  startOfToday
 } from "date-fns";
 import { useState } from "react";
 import { userContext } from "./userContext";
@@ -37,7 +37,7 @@ function classNames(...classes: (string | boolean)[]) {
 export function LogsPage(props: LogsProps) {
   const { clientId } = useContext(userContext);
   const [logs, setLogs] = useState([
-    { userId: "", date: "", prompt: "", response: "", mood: "" },
+    { userId: "", date: "", prompt: "", response: "", mood: "" }
   ]);
 
   useEffect(() => {
@@ -47,10 +47,10 @@ export function LogsPage(props: LogsProps) {
           "http://localhost:3001/api/log/findUser?" +
             new URLSearchParams({ userId: clientId }),
           {
-            method: "GET",
+            method: "GET"
           }
-        ).then((response) => {
-          return response.json().then((response) => {
+        ).then(response => {
+          return response.json().then(response => {
             console.log("response: ", response);
             setLogs(response);
           });
@@ -85,7 +85,7 @@ export function LogsPage(props: LogsProps) {
 
   let days = eachDayOfInterval({
     start: firstDayCurrentMonth,
-    end: endOfMonth(firstDayCurrentMonth),
+    end: endOfMonth(firstDayCurrentMonth)
   });
 
   function previousMonth() {
@@ -118,12 +118,12 @@ export function LogsPage(props: LogsProps) {
   }
 
   function findLog(): string {
-    let res = logs.find((log) => isSameDay(parseISO(log.date), selectedDay));
+    let res = logs.find(log => isSameDay(parseISO(log.date), selectedDay));
     return res ? res.response : "";
   }
 
-  function findPrompt() : string {
-    let res = logs.find((log) => isSameDay(parseISO(log.date), selectedDay));
+  function findPrompt(): string {
+    let res = logs.find(log => isSameDay(parseISO(log.date), selectedDay));
     return res ? res.prompt : "";
   }
 
@@ -137,85 +137,85 @@ export function LogsPage(props: LogsProps) {
 
   return (
     <div
-      className="flex place-content-center bg-secondary-bg rounded-full flex flex-col justify-around items-center focus:outline-none"
+      className='flex place-content-center bg-secondary-bg rounded-full flex flex-col justify-around items-center focus:outline-none'
       tabIndex={0}
     >
-      <div className="bg-white p-2 rounded-3xl flex place-content-center mt-[5%] mb-[5%]">
+      <div className='bg-white p-2 rounded-3xl flex place-content-center mt-[5%] mb-[5%]'>
         {isDayView ? (
-          <div className="bg-yellow-100 rounded-3xl border-2 border-black p-2">
-            <div className="bg-yellow-100 rounded-3xl border border-1 border-black h-fit p-4">
-              <div className="flex justify-end">
+          <div className='bg-yellow-100 rounded-3xl border-2 border-black p-2'>
+            <div className='bg-yellow-100 rounded-3xl border border-1 border-black h-fit p-4'>
+              <div className='flex justify-end'>
                 <button
-                  type="button"
-                  className="rounded-md sm border border-solid border-1 border-black w-7 h-7 m-2 flex place-content-center"
+                  type='button'
+                  className='rounded-md sm border border-solid border-1 border-black w-7 h-7 m-2 flex place-content-center'
                   onClick={handleBack}
                 >
-                  <XIcon className="w-6 h-6" aria-hidden="true" />
+                  <XIcon className='w-6 h-6' aria-hidden='true' />
                 </button>
               </div>
-              <div className="flex flex-row">
-                <div className="flex-none hidden lg:block box-border rounded-lg bg-white h-24 w-24 border-solid border-2 border-black mb-2">
-                  <img src={DinoPfp} alt="dino pic" className="rounded-lg" />
+              <div className='flex flex-row'>
+                <div className='flex-none hidden lg:block box-border rounded-lg bg-white h-24 w-24 border-solid border-2 border-black mb-2'>
+                  <img src={DinoPfp} alt='dino pic' className='rounded-lg' />
                 </div>
-                <p className="flex inline items-start font-semibold text-black text-base p-1 pl-6 text-inherit max-w-[70%]">
+                <p className='flex inline items-start font-semibold text-black text-base p-1 pl-6 text-inherit max-w-[70%]'>
                   {findPrompt()}
                 </p>
               </div>
               <textarea
                 rows={9}
-                placeholder="type response here"
-                className="p-2.5 text-base rounded-lg text-inherit resize-none w-full border-solid border-2 border-black"
+                placeholder='type response here'
+                className='p-2.5 text-base rounded-lg text-inherit resize-none w-full border-solid border-2 border-black'
                 value={findLog()}
                 readOnly={true}
               ></textarea>
-              <div className="flex flex-row justify-around pt-2">
+              <div className='flex flex-row justify-around pt-2'>
                 <button
-                  type="button"
-                  className="rounded-md w-20 m-2 flex place-content-center"
+                  type='button'
+                  className='rounded-md w-20 m-2 flex place-content-center'
                   onClick={previousDay}
                 >
-                  <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
+                  <ChevronLeftIcon className='w-5 h-5' aria-hidden='true' />
                 </button>
-                <div className="font-mono text-lg font-bold pt-2">
+                <div className='font-mono text-lg font-bold pt-2'>
                   {format(selectedDay, "MMM-dd-yyyy").toString()}
                 </div>
                 <button
-                  type="button"
-                  className="rounded-md h-15 w-20 m-2 flex place-content-center"
+                  type='button'
+                  className='rounded-md h-15 w-20 m-2 flex place-content-center'
                   onClick={nextDay}
                 >
-                  <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+                  <ChevronRightIcon className='w-5 h-5' aria-hidden='true' />
                 </button>
               </div>
             </div>
           </div>
         ) : (
-          <div className="aspect-square box-content rounded-3xl flex flex-col max-w-fit max-h-fit p-3 pt-4 bg-main-bg">
-            <div className="flex items-center">
+          <div className='aspect-square box-content rounded-3xl flex flex-col max-w-fit max-h-fit p-3 pt-4 bg-main-bg'>
+            <div className='flex items-center'>
               <button
-                type="button"
+                type='button'
                 onClick={previousMonth}
-                className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-white hover:text-gray-500"
+                className='-my-1.5 flex flex-none items-center justify-center p-1.5 text-white hover:text-gray-500'
               >
-                <span className="sr-only">Previous month</span>
-                <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
+                <span className='sr-only'>Previous month</span>
+                <ChevronLeftIcon className='w-5 h-5' aria-hidden='true' />
               </button>
-              <h2 className="flex-auto text-center font-semibold text-white pb-4">
+              <h2 className='flex-auto text-center font-semibold text-white pb-4'>
                 {format(firstDayCurrentMonth, "MMMM yyyy")}
               </h2>
 
               <button
                 onClick={nextMonth}
-                type="button"
-                className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-white hover:text-gray-500"
+                type='button'
+                className='-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-white hover:text-gray-500'
               >
-                <span className="sr-only">Next month</span>
-                <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+                <span className='sr-only'>Next month</span>
+                <ChevronRightIcon className='w-5 h-5' aria-hidden='true' />
               </button>
             </div>
-            <div className="rounded-3xl max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-10 font-mono font-bold bg-white">
-              <div className="md:pr-0 flex flex-col mr-0 justify-center pb-6">
-                <div className="grid grid-cols-7 gap-x-3 mt-10 text-md leading-6 text-center text-main-bg">
+            <div className='rounded-3xl max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-10 font-mono font-bold bg-white'>
+              <div className='md:pr-0 flex flex-col mr-0 justify-center pb-6'>
+                <div className='grid grid-cols-7 gap-x-3 mt-10 text-md leading-6 text-center text-main-bg'>
                   <div>sun</div>
                   <div>mon</div>
                   <div>tue</div>
@@ -224,7 +224,7 @@ export function LogsPage(props: LogsProps) {
                   <div>fri</div>
                   <div>sat</div>
                 </div>
-                <div className="grid grid-cols-7 mt-2 text-lg">
+                <div className='grid grid-cols-7 mt-2 text-lg'>
                   {days.map((day, dayIdx) => (
                     <div
                       key={day.toString()}
@@ -240,21 +240,19 @@ export function LogsPage(props: LogsProps) {
                 */}
 
                       <button
-                        type="button"
+                        type='button'
                         onClick={() => {
                           setSelectedDay(day);
                           handleClick();
                         }}
                         className={classNames(
                           hasLog(day) === 0 &&
-                            "bg-green-300 text-white font-semibold",
+                            "bg-green text-white font-semibold",
                           hasLog(day) === 1 &&
-                            "bg-yellow-600 text-white font-semibold",
+                            "bg-yellow text-white font-semibold",
                           hasLog(day) === 2 &&
-                            "bg-main-bg text-white font-semibold",
-                          isEqual(day, selectedDay) &&
-                            !isToday(day) &&
-                            "text-red-500",
+                            "bg-red text-white font-semibold",
+                          isEqual(day, selectedDay) && !isToday(day),
                           isToday(day) &&
                             "outline outline-4 outline-secondary-bg",
                           !isEqual(day, selectedDay) &&
@@ -275,7 +273,7 @@ export function LogsPage(props: LogsProps) {
           </div>
         )}
       </div>
-      <div className="hidden md:flex flex-row w-3/4 justify-center pb-5 space-x-4"></div>
+      <div className='hidden md:flex flex-row w-3/4 justify-center pb-5 space-x-4'></div>
     </div>
   );
 }
@@ -287,7 +285,7 @@ let colStartClasses = [
   "col-start-4",
   "col-start-5",
   "col-start-6",
-  "col-start-7",
+  "col-start-7"
 ];
 
 // source: https://www.youtube.com/watch?v=9ySmMd5Cjc0
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 module.exports = {
-  content: ["./src/**/*.{html, js, tsx}",
-            "./public/**/*.{html, js, tsx}",
-            "./public/index.html",
-            "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{html, js, tsx}",
+    "./public/**/*.{html, js, tsx}",
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {
       width: {
-        '80%' : '80%',
-        '90%' : '90%'
+        "80%": "80%",
+        "90%": "90%"
       }
     },
     colors: {
-        'main-bg': '#DF7E74',
-        'secondary-bg': '#F9AE91',
-        'white': colors.white,
-        'black': colors.black,
-        'red': colors.red,
-        'blue': colors.blue,
-        'slate': colors.slate,
-        'stone': colors.stone,
-        'green': colors.green,
-        'yellow': colors.yellow,
-        'gray': colors.gray,
-        'btn-yellow': 'FBF7BF'
-    },
+      "main-bg": "#DF7E74",
+      "secondary-bg": "#F9AE91",
+      white: colors.white,
+      black: colors.black,
+      red: {
+        DEFAULT: "#DF7E74",
+        dark: "#BA6A62"
+      },
+      blue: colors.blue,
+      slate: colors.slate,
+      stone: colors.stone,
+      green: {
+        DEFAULT: "#8DC692",
+        dark: "#70A175"
+      },
+      yellow: {
+        DEFAULT: "#F5BF6D",
+        dark: "#D39A42"
+      },
+      gray: colors.gray,
+      "logs-green": "#8DC692"
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
